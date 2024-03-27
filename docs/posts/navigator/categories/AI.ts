d2ced82c55329aab069e41599b28d4ce6110c0b4
super456 <superdu456@163.com>
2024-03-27 11:06:25 +0800

    feat: 添加AI导航信息

--- docs/posts/navigator/categories/AI.ts
+++ docs/posts/navigator/categories/AI.ts
@@ -5,7 +5,7 @@ export default {
     {
       icon: '/icons/chatgpt.png',
       title: 'ChatGPT',
-      desc: '聊天',
+      desc: 'AI 聊天对话',
       link: 'https://chat.openai.com/chat',
     },
     {
@@ -15,34 +15,64 @@ export default {
       link: 'https://kimi.moonshot.cn/',
     },
     {
+      icon: 'https://metaso.cn/favicon.ico',
+      title: '秘塔 AI 搜索',
+      desc: '知识类内容检查，没有广告直达结果，分析深入',
+      link: 'https://metaso.cn/',
+    },
+    {
+      icon: 'https://static.tiangong.cn/mainsite/logo.ico',
+      title: '天工 AI',
+      desc: '「天工」是国内首个对标 ChatGPT 的双千亿级大语言模型，也是一个对话式AI助手。「天工」通过自然语言与用户进行问答交互，AI 生成能力可满足文案创作、知识问答、逻辑推演、数理推算、代码编程等多元化需求。支持1万字以上文本对话，实现20轮次以上用户交互，在学习、职场、生活等多类问答场景中都能实现较高的输出水平。',
+      link: 'https://beta.tiangong.cn/',
+    },
+    {
+      icon: 'https://nlp-eb.cdn.bcebos.com/logo/favicon.ico',
+      title: '文心一言',
+      desc: '既能写文案、读文档，又能脑洞大开、答疑解惑，还能倾听你的故事、感受你的心声。快来和我对话吧！',
+      link: 'https://yiyan.baidu.com/welcome',
+    },
+    {
       icon: 'https://www.notion.so/images/logo-ios.png',
       title: 'Notion AI',
-      desc: '笔记',
+      desc: 'AI 输入笔记',
       link: 'https://www.notion.so',
     },
     {
       icon: 'https://www.midjourney.com/apple-touch-icon.png',
       title: 'Midjourney',
-      desc: '绘画',
+      desc: 'AI 绘画',
       link: 'https://www.midjourney.com',
     },
     {
       icon: 'https://stablediffusionweb.com/images/logo.png',
       title: 'Stable Diffusion Online',
-      desc: '绘画',
+      desc: 'AI 绘画',
       link: 'https://stablediffusionweb.com/',
     },
     {
       icon: 'https://global-uploads.webflow.com/59deb588800ae30001ec19c9/5d4891e0e260e3c1bc37b100_beautiful%20ai%20favicon%20%20blue%20square.png',
       title: 'Beautiful.ai',
-      desc: 'PPT',
+      desc: 'AI 生成PPT',
       link: 'https://www.beautiful.ai',
+    },
+    {
+      icon: 'https://www.suno.ai/favicon.svg',
+      title: 'Suno AI',
+      desc: 'AI 生成音乐',
+      link: 'https://www.suno.ai/',
     },
     {
       icon: 'https://img.alicdn.com/imgextra/i1/O1CN01BN6Jtc1lCfJNviV7H_!!6000000004783-2-tps-134-133.png',
       title: '通义灵码',
       desc: '阿里云通义灵码，你的智能编码助手，为开发者提供行级和函数级代码续写、单元测试生成、代码注释生成、研发智能问答等能力，助你高质高效地完成编码工作',
       link: 'https://tongyi.aliyun.com/lingma',
+    },
+    {
+      icon: 'https://g.alicdn.com/idst-fe/mind-meeting-assistant2/0.0.198/favicon.png',
+      title: '通义听悟',
+      desc: '阿里云通义听悟是聚焦音视频内容的工作学习AI助手，依托大模型，帮助用户记录、整理和分析音视频内容，体验用大模型做音视频笔记、整理会议记录。',
+      link: 'https://tingwu.aliyun.com/',
     },
     {
       icon: 'https://comate.baidu.com/images/favicon.ico',
