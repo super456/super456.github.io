9cb3f8fa1c19c6f2a500eca23b3c97b3a5e1f39c
super456 <superdu456@163.com>
2024-11-03 21:49:05 +0800

    feat: 更新朝花夕拾第 63 本书内容

--- docs/.vitepress/sidebar/zhaohuaxishi.ts
+++ docs/.vitepress/sidebar/zhaohuaxishi.ts
@@ -5,6 +5,7 @@ export default function zhaohuaxishiSidebarGuide() {
       text: '2024 年',
       collapsed: false,
       items: [
+        { text: '第 63 本《埃隆·马斯克传》', link: '/posts/read-book/2024/elon-musk' },
         { text: '第 62 本《Electron 实战：入门、进阶与性能优化》', link: '/posts/read-book/2024/electron-tutorial' },
         { text: '第 61 本《深入浅出 Vue.js》', link: '/posts/read-book/2024/simple-easy-understand-vue' },
         { text: '第 60 本《深入浅出 Electron 原理、工程与实践》', link: '/posts/read-book/2024/simple-easy-understand-electron' },
